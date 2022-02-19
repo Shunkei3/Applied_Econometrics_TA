f9b646a1a60e0575f28344690c4195bb8e301860
Shun <skakimoto3@huskers.unl.edu>
2022-02-18 19:29:18 -0600

    materials for session Feb18

---
+++ run_rmd.R
@@ -0,0 +1,8 @@
+library(here)
+library(rmarkdown)
+
+# === Assignment 1 === #
+rmarkdown::render(here("GitControlled", "Assignment_1", "Review1_slides.rmd"))
+
+# === Assignment 2 === #
+rmarkdown::render(here("GitControlled", "Assignment_2", "Review1_slides.rmd"))
